6bafbf9b8d536cbfc75d6510115ab5321950ac20
Ing-Kalebb <kalebbfuentes@gmail.com>
2026-09-03 12:58:48 -0600

    feat: Archivos despues de logearse

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,8 +27,26 @@ export default App;*/
 
 import React, { useState } from "react";
 import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+
 function App() {
-  return <Login />;
+  const [usuario, setUsuario] = useState(null);
+
+  const handleLogin = (usuarioData) => {
+    setUsuario(usuarioData);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setUsuario(null);
+  };
+
+  if (!usuario) {
+    return <Login onLogin={handleLogin} />;
+  }
+
+  return <Dashboard usuario={usuario} onLogout={handleLogout} />;
 }
 
 export default App;
+
