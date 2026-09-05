6f60a57e8012555f6940d4954393b48b6e42dbbc
Ing-Kalebb <kalebbfuentes@gmail.com>
2026-09-04 19:30:01 -0600

    Feat: save png signature

--- backend/app/config/upload.js
+++ backend/app/config/upload.js
@@ -3,14 +3,14 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Carpeta donde se guardarán las imágenes
+    cb(null, path.join(__dirname, "../uploads")); // 👈 apunta a backend/app/uploads
   },
   filename: (req, file, cb) => {
-    // Mantener extensión original
     const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1E9);
     cb(null, uniqueName + path.extname(file.originalname));
   }
 });
+
 
 const upload = multer({ storage });
 
